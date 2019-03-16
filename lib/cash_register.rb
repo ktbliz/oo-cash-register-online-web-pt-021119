f1239b4c578a83646f794501feca246e651308b4
Katie Blizzard <kblizzard11@gmail.com>
2019-03-16 20:10:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   end 
   
   def add_item(item, price, quantity = 1)
-    @total += (price*quantity)
+    self.total += (price*quantity)
     quantity.times do 
       @items.push(item) 
     end 
